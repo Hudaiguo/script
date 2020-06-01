4bc156497a64203fd6f522436add5ce70a7c8baf
Administrator <suiyueliusha2013@163.com>
2020-06-01 17:28:00 +0800

    cv2学习

--- learn_code_function/图像相关函数.py
+++ learn_code_function/图像相关函数.py
@@ -11,7 +11,6 @@ import numpy as np
 #从指定的内存缓存中读取数据，并把数据转换(解码)成图像格式;主要用于从网络传输数据中恢复出图像。
 cv2.imdecode(buf=, flags=)
 #将图片格式转换(编码)成流数据，赋值到内存缓存中;主要用于图像数据格式的压缩，方便网络传输。
-cv2.imencode()
 cv2.imencode(ext=, img=, params=)
 
 
